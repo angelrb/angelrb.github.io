df343993159af3de3f672916c150512578f154aa
angelrb <fs_254@hotmail.com>
2016-10-20 16:00:36 -0500

    Update ejemplo201016.js

--- rv/ejemplo201016.js
+++ rv/ejemplo201016.js
@@ -35,7 +35,7 @@ pieza.escena.add(pieza);
 function loop(){
 requestAnimationFrame(loop);
 pieza.rotateY(0.02);
-pieza.piernaIzq.rotateX(0.05);
+pieza.piernaIzq.rotateX(-0.05);
 pieza.piernaDer.rotateX(0.05);
 pieza.renderizador.render(pieza.escena,pieza.camara);
 }
