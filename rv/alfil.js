b3f8d3226b9efec7d89239b495c48440e66c180c
angelrb <fs_254@hotmail.com>
2016-11-22 23:19:16 -0600

    Update alfil.js

--- rv/alfil.js
+++ rv/alfil.js
@@ -4,12 +4,7 @@ var base2 = new THREE.CylinderGeometry(0.8, 0.8, 0.2, 50, 25);
 var base3 = new THREE.CylinderGeometry(0.5, 0.7, 1.5, 50, 25); 
 var base4 = new THREE.CylinderGeometry(0.3, 0.6, 0.2, 50, 25); 
 var base5 = new THREE.CylinderGeometry(0.6, 0.6, 0.1, 50, 25); 
-//var cima = new THREE.SphereGeometry( 0.55, 32, 32 );
-var points = [];
-for ( var i = 0; i < 10; i ++ ) {
-	points.push( new THREE.Vector2( Math.sin( i * 0.2 ), ( i - 5 )  ) );
-}
-var cima = new THREE.LatheGeometry( points );
+var cima = new THREE.SphereGeometry( 0.55, 32, 32 );
 var cima1 = new THREE.SphereGeometry( 0.1, 32, 32 );
 
 
