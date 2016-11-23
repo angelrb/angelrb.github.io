9d51eb55eee95b859a22e8446fab8c32cc7a0884
angelrb <fs_254@hotmail.com>
2016-11-22 22:43:13 -0600

    Update alfil.js

--- rv/alfil.js
+++ rv/alfil.js
@@ -3,6 +3,8 @@ var base1 = new THREE.CylinderGeometry(0.7, 0.8, 0.2, 50, 25);
 var base2 = new THREE.CylinderGeometry(0.8, 0.8, 0.2, 50, 25); 
 var base3 = new THREE.CylinderGeometry(0.6, 0.7, 1, 50, 25); 
 var base4 = new THREE.CylinderGeometry(0.5, 0.8, 0.2, 50, 25); 
+var base5 = new THREE.CylinderGeometry(0.6, 0.6, 0.1, 50, 25); 
+
 //var cima = new THREE.SphereGeometry( 0.4, 32, 32 ); 
 
 
@@ -11,12 +13,14 @@ base1.translate(0, 0.2, 0);
 base2.translate(0, 0.4, 0); 
 base3.translate(0, 0.6, 0); 
 base4.translate(0, 1.2, 0); 
+base5.translate(0, 1.4, 0);
  
 var baseMalla = new THREE.Mesh(base); 
 var base1Malla = new THREE.Mesh(base1); 
 var base2Malla = new THREE.Mesh(base2); 
 var base3Malla = new THREE.Mesh(base3); 
 var base4Malla = new THREE.Mesh(base4); 
+var base5Malla = new THREE.Mesh(base5); 
  
 var peonForma = new THREE.Geometry(); 
  
@@ -25,6 +29,7 @@ peonForma.merge(base1Malla.geometry,base1Malla.matrix);
 peonForma.merge(base2Malla.geometry,base2Malla.matrix); 
 peonForma.merge(base3Malla.geometry,base3Malla.matrix); 
 peonForma.merge(base4Malla.geometry,base4Malla.matrix); 
+peonForma.merge(base5Malla.geometry,base5Malla.matrix); 
  
 var material = new THREE.MeshNormalMaterial(); 
 var peonMalla = new THREE.Mesh(peonForma,material); 
