e1298f2e2ecf407b94eed1caa161159eb65eedcb
angelrb <fs_254@hotmail.com>
2016-11-22 22:59:03 -0600

    Update alfil.js

--- rv/alfil.js
+++ rv/alfil.js
@@ -7,13 +7,13 @@ var base5 = new THREE.CylinderGeometry(0.7, 0.7, 0.1, 50, 25);
 var cima = new THREE.SphereGeometry( 0.65, 32, 32 ); 
 
 
-base.translate(0, 0, 0); 
-base1.translate(0, 0.2, 0); 
-base2.translate(0, 0.4, 0); 
-base3.translate(0, 0.6, 0); 
-base4.translate(0, 1.2, 0); 
-base5.translate(0, 1.3, 0);
-cima.translate(0, 1.8, 0);
+base.translate(0, -1, 0); 
+base1.translate(0, -0.8, 0); 
+base2.translate(0, -0.6, 0); 
+base3.translate(0, -0.4, 0); 
+base4.translate(0, 0.2, 0); 
+base5.translate(0, 0.3, 0);
+cima.translate(0, 0.8, 0);
  
 var baseMalla = new THREE.Mesh(base); 
 var base1Malla = new THREE.Mesh(base1); 
@@ -40,7 +40,7 @@ peonMalla.rotateX(Math.PI/8);
 var escena = new THREE.Scene(); 
 escena.add(peonMalla); 
 var camara = new THREE.PerspectiveCamera(); 
-camara.position.z=7; 
+camara.position.z=5; 
 var renderizador = new THREE.WebGLRenderer(); 
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95); 
 document.body.appendChild(renderizador.domElement); 
