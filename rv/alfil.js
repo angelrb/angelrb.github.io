c43cda534d7ff07988fda2d7492dd405c65006f0
angelrb <fs_254@hotmail.com>
2016-11-22 22:56:55 -0600

    Update alfil.js

--- rv/alfil.js
+++ rv/alfil.js
@@ -40,7 +40,7 @@ peonMalla.rotateX(Math.PI/8);
 var escena = new THREE.Scene(); 
 escena.add(peonMalla); 
 var camara = new THREE.PerspectiveCamera(); 
-camara.position.z=5; 
+camara.position.z=7; 
 var renderizador = new THREE.WebGLRenderer(); 
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95); 
 document.body.appendChild(renderizador.domElement); 
