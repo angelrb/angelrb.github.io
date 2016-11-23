1ce4d177b305fa26a0af7edb6586299a1dbd842e
angelrb <fs_254@hotmail.com>
2016-11-22 23:00:31 -0600

    Update alfil.js

--- rv/alfil.js
+++ rv/alfil.js
@@ -1,7 +1,7 @@
 var base = new THREE.CylinderGeometry(0.9, 0.9, 0.2, 50, 25); 
 var base1 = new THREE.CylinderGeometry(0.7, 0.8, 0.2, 50, 25);
 var base2 = new THREE.CylinderGeometry(0.8, 0.8, 0.2, 50, 25); 
-var base3 = new THREE.CylinderGeometry(0.6, 0.7, 1, 50, 25); 
+var base3 = new THREE.CylinderGeometry(0.6, 0.7, 1.5, 50, 25); 
 var base4 = new THREE.CylinderGeometry(0.5, 0.8, 0.2, 50, 25); 
 var base5 = new THREE.CylinderGeometry(0.7, 0.7, 0.1, 50, 25); 
 var cima = new THREE.SphereGeometry( 0.65, 32, 32 ); 
@@ -11,9 +11,9 @@ base.translate(0, -1, 0);
 base1.translate(0, -0.8, 0); 
 base2.translate(0, -0.6, 0); 
 base3.translate(0, -0.4, 0); 
-base4.translate(0, 0.2, 0); 
-base5.translate(0, 0.3, 0);
-cima.translate(0, 0.8, 0);
+base4.translate(0, 0.7, 0); 
+base5.translate(0, 0.8, 0);
+cima.translate(0, 1.3, 0);
  
 var baseMalla = new THREE.Mesh(base); 
 var base1Malla = new THREE.Mesh(base1); 
