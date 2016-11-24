76fc3341973a22f22a1188387304c656b1b8bba3
angelrb <fs_254@hotmail.com>
2016-11-23 21:33:26 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -1,7 +1,7 @@
 var base = new THREE.CylinderGeometry(0.9, 0.9, 0.2, 50, 25); 
-var base1 = new THREE.CylinderGeometry(0.7, 0.9, 0.4, 50, 25);
-var base2 = new THREE.CylinderGeometry(0.7, 0.7, 0.3, 50, 25); 
-var base3 = new THREE.CylinderGeometry(0.4, 0.7, 1, 50, 25); 
+var base1 = new THREE.CylinderGeometry(0.7, 1, 0.4, 50, 25);
+var base2 = new THREE.CylinderGeometry(0.7, 0.7, 0.2, 50, 25); 
+var base3 = new THREE.CylinderGeometry(0.4, 0.7, 2, 50, 25); 
 //var base4 = new THREE.CylinderGeometry(0.3, 0.6, 0.2, 50, 25); 
 //var base5 = new THREE.CylinderGeometry(0.6, 0.6, 0.1, 50, 25); 
 //var cima = new THREE.SphereGeometry( 0.55, 32, 32 );
