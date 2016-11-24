0e39ecc86c68bb9e51226e57df30cdc813179d89
angelrb <fs_254@hotmail.com>
2016-11-23 22:17:06 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -6,6 +6,7 @@ var base4 = new THREE.TorusGeometry( 0.8, 0.1, 16, 100 );
 var base5 = new THREE.CylinderGeometry(0.8, 0.8, 0.15, 50, 25);
 var base6 = new THREE.TorusGeometry( 0.6, 0.1, 16, 100 );
 var base7 = new THREE.CylinderGeometry(0.6, 0.6, 0.3, 50, 25);
+var base8 = new THREE.TorusGeometry( 0.6, 0.1, 16, 100 );
 //var cima = new THREE.SphereGeometry( 0.55, 32, 32 );
 //var cima1 = new THREE.SphereGeometry( 0.1, 32, 32 );
 
@@ -20,6 +21,8 @@ base5.translate(0, 1.7, 0);
 base6.rotateX(Math.PI/2);
 base6.translate(0, 1.8, 0);
 base7.translate(0, 1.9, 0);
+base8.rotateX(Math.PI/2);
+base8.translate(0, 2, 0);
 //cima.translate(0, 0.9, 0);
 //cima1.translate(0, 1.5, 0);
  
@@ -31,6 +34,7 @@ var base4Malla = new THREE.Mesh(base4);
 var base5Malla = new THREE.Mesh(base5); 
 var base6Malla = new THREE.Mesh(base6); 
 var base7Malla = new THREE.Mesh(base7); 
+var base8Malla = new THREE.Mesh(base8); 
 //var cimaMalla = new THREE.Mesh(cima); 
 //var cima1Malla = new THREE.Mesh(cima1); 
  
@@ -44,6 +48,7 @@ alfil.merge(base4Malla.geometry,base4Malla.matrix);
 alfil.merge(base5Malla.geometry,base5Malla.matrix); 
 alfil.merge(base6Malla.geometry,base6Malla.matrix); 
 alfil.merge(base7Malla.geometry,base7Malla.matrix); 
+alfil.merge(base8Malla.geometry,base8Malla.matrix); 
 //alfil.merge(cimaMalla.geometry,cimaMalla.matrix); 
 //alfil.merge(cima1Malla.geometry,cima1Malla.matrix);
 
