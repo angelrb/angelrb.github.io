80fc6a757903a9eee5db80744a1524c6ab831146
angelrb <fs_254@hotmail.com>
2016-11-23 21:28:26 -0600

    Update rey.js

--- rv/rey.js
+++ rv/rey.js
@@ -44,7 +44,7 @@ alfil.rotateX(Math.PI/8);
 var escena = new THREE.Scene(); 
 escena.add(alfilMalla); 
 var camara = new THREE.PerspectiveCamera(); 
-camara.position.z=5; 
+camara.position.z=1; 
 var renderizador = new THREE.WebGLRenderer(); 
 renderizador.setSize(window.innerHeight*.95,window.innerHeight*.95); 
 document.body.appendChild(renderizador.domElement); 
